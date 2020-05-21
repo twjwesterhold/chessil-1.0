e0aa089d54046e22d61d543ee1e10311aec9c933
twjwesterhold <thomas.westerhold@vanderbilt.edu>
2020-05-21 02:57:30 -0400

    First commit, added .gitignore

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+var FILES = 'abcdefgh'.split;
+
+function App() {
+  return (
+    <Board />
+  );
+}
+
+function Board() {
+  return (
+    <div>
+    hello
+    </div>
+    );
+}
+
+function Square() {
+  
+}
+
+export default App;
