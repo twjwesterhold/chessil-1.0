9b04ddfe94c5b2b90ba2234cf93ce8fbf7874b10
twjwesterhold <thomas.westerhold@vanderbilt.edu>
2020-05-21 23:38:55 -0400

    Restructuring project with stronger component organization and implementing routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
 
 const App = () => {
   return (
+    <Router>
     <Board squares={DEFAULT_BOARD} />
+    <Router />
   );
 };
 
